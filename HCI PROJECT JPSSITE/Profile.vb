30d8376949e1747725cc849644e673dcdc2fc55e
AetherKnowledge <152044019+AetherKnowledge@users.noreply.github.com>
2024-05-18 03:24:33 +0800

    Sidebar button functionality Profile Changes depending on user

--- HCI PROJECT JPSSITE/Profile.vb
+++ HCI PROJECT JPSSITE/Profile.vb
@@ -1,4 +1,20 @@
 ﻿Public Class Profile
+    Shadows Sub show()
+        MyBase.Show()
+        fullnameLabel.Text = UserHandler.getCurrentuser.firstName + " " + UserHandler.getCurrentuser.surName
+        usernameLabel.Text = UserHandler.getCurrentuser.username
+        userIDLabel.Text = UserHandler.getCurrentuser.userID
+        usrnameLabel.Text = UserHandler.getCurrentuser.username
+        courselabel.Text = UserHandler.getCurrentuser.courseProgram
+        yearLabel.Text = UserHandler.getCurrentuser.yearLevel
+        passLabel.Text = "*******"
+
+        firstnameField.Text = UserHandler.getCurrentuser.firstName
+        surnameField.Text = UserHandler.getCurrentuser.firstName
+        usernameField.Text = UserHandler.getCurrentuser.username
+        birthDateField.Value = UserHandler.getCurrentuser.birthDate
+        passwordField.Text = "Password"
+    End Sub
 
     Private Sub dashboardBtn_Click(sender As Object, e As EventArgs) Handles dashboardBtn.Click
         Me.Hide()
@@ -7,12 +23,12 @@
 
     Private Sub eventsBtn_Click(sender As Object, e As EventArgs) Handles eventsBtn.Click
         Me.Hide()
-        HCI_PROJECT_JPSSITE.Events.show()
+        HCI_PROJECT_JPSSITE.Events.Show()
     End Sub
 
     Private Sub ratingsBtn_Click(sender As Object, e As EventArgs) Handles ratingsBtn.Click
         Me.Hide()
-        Ratings.show()
+        Ratings.Show()
     End Sub
 
     Private Sub officersBtn_Click(sender As Object, e As EventArgs) Handles officersBtn.Click
@@ -22,5 +38,9 @@
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
 
+    End Sub
+
+    Private Sub Me_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Login.Close()
     End Sub
 End Class
